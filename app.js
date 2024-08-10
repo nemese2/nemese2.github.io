516d68211433cf19e7dffa8519f623d164cb0f42
nemese <nemese2@proton.me>
2024-08-10 03:18:58 -0700

    Add files via upload
    Upload project roots to GitHub.

---
+++ app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.bar-menu')
+const scroll = document.querySelector('.head-image')
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active')
+  menuLinks.classList.toggle('active');
+})
+
+window.addEventListener('scroll', function(){
+  var value = 0 - window.scrollY * 0.8;
+  scroll.style.transform = `translateY(${value}px)`;
+})
