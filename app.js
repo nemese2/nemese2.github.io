5764537d14947a0694d4d29d759c0f9072d07f13
nemese2 <nemese2@proton.me>
2024-08-15 21:29:49 -0700

    Fix landing page parallax algorithm.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const menu = document.querySelector('.bar-toggle')
 const menuLinks = document.querySelector('.bar-menu')
 const scroll = document.querySelector('.head-image')
-const width = document.body.clientWidth
 
 menu.addEventListener('click', function() {
   menu.classList.toggle('is-active')
   menuLinks.classList.toggle('active');
 })
 
+setInterval(function(){
+  window.width = document.body.clientWidth;
+}, 10)
+
 window.addEventListener('scroll', function(){
-  var value = 0 - (window.scrollY * (300 / width));
+  var value = 0 - ((window.scrollY * (window.width * 0.02 + 40)) * 0.01);
   scroll.style.transform = `translateY(${value}px)`;
 })
